7efc825bb233b6b31fd22ad73eb2394d7a6e299e
shine_forever <shine_forever@yeah.net>
2014-11-10 23:47:11 +0800

    修改model

--- blog/admin.py
+++ blog/admin.py
@@ -8,14 +8,16 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name','article_num')
 
 
+
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('caption','category','ceatetime','hits','times','goods','bads')
+    list_display = ('caption','category','createtime','hits','times','goods','bads')
+
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('article','content','createtime','name','email')
 
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Article,ArticleAdmin)
-admin.site.register(Comment,CategoryAdmin)
+admin.site.register(Comment,CommentAdmin)
 
 
