4221360fb321a8642dda8a1328d26c06f836a2a6
shine_forever <shine_forever@yeah.net>
2014-11-11 22:32:26 +0800

    配置setting文件，自动匹配不同环境.

--- manage.py
+++ manage.py
@@ -2,10 +2,22 @@
 #coding:utf-8
 import os
 import sys
+import socket
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
+    """
+    配置两种环境中setting文件，自动匹配;
+    """
+    if socket.gethostname() == 'guolt-pythoner':
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings_pythoner")
 
-    from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+        execute_from_command_line(sys.argv)
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
+
+        from django.core.management import execute_from_command_line
+
+        execute_from_command_line(sys.argv)
+        
